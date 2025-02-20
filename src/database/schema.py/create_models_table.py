5f36a22c0e70336024ebbf6f6a006f098ea92da8
Zhining Li <zhiningli.dev@gmail.com>
2025-02-20 22:24:16 +0000

    models table creation

---
+++ src/database/schema.py/create_models_table.py
@@ -0,0 +1,23 @@
+from src.database.connection import get_connection
+
+
+def create_models_table():
+    """Crete models table in postgresql"""
+
+    query = """
+    CREATE TABLE IF NOT EXISTS models (
+    model_idx SERIAL PRIMARY KEY,
+    code JSONB NOT NULL,
+    feature_vector REAL[],
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+    """
+
+    with get_connection() as conn:
+        with conn.cursor() as cursor:
+            cursor.execute(query)
+            conn.commit()
+
+
+if __name__ == "__main__":
+    create_models_table()
