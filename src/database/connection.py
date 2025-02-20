40bb00664d6f7ec0707dfbb21484e67d5af67899
Zhining Li <zhiningli.dev@gmail.com>
2025-02-20 18:45:23 +0000

    postgresql connection

--- src/database/connection.py
+++ src/database/connection.py
@@ -1,3 +1,23 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL, echo = True)
+
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+
+Base = declarative_base()
+
+def get_db():
+    """Dependency for database session"""
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
