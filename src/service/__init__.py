7f7b02f5b2e9a886b7e1ea6af7aff7eb97f933ff
Zhining Li <zhiningli.dev@gmail.com>
2025-02-22 22:59:29 +0000

    init similarity inference service

--- src/service/__init__.py
+++ src/service/__init__.py
@@ -1,6 +1,7 @@
 from .hyperparameter_evaluation_service import HPEvaluationService
 from .similarity_training_service import SimilarityTrainingService
+from .similarity_inference_service import SimilarityInferenceService
 from .bo_service import BOService
 from .embeddings import *
 
-__all__ = ["HPEvaluationService", "SimilarityTrainingService", "BOService"]
+__all__ = ["HPEvaluationService", "SimilarityTrainingService", "BOService", "SimilarityInferenceService"]
---
+++ src/service/similarity_inference_service.py
@@ -0,0 +1,6 @@
+
+
+class SimilarityInferenceService:
+
+    def __init__(self):
+        pass
